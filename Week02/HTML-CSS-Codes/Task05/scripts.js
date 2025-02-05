f2584cba86f2238ae86d2346e7ea8e57f8c0b44e
sulemaniftikhar <sulemanbhatti1810@gmail.com>
2025-02-05 18:33:37 +0500

    added task 5
    resume

---
+++ Week02/HTML-CSS-Codes/Task05/scripts.js
@@ -0,0 +1,18 @@
+
+// Scroll Animation
+const sections = document.querySelectorAll('.section');
+const options = {
+    threshold: 0.3
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
